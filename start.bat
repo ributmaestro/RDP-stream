2ad6004e910d521f1a5c9f630f5463c519be25da
ributmaestro <88657490+ributmaestro@users.noreply.github.com>
2021-08-09 17:06:53 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -16,6 +16,6 @@ echo Successfully Installed!, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: @7onezRDP2021
+echo Password: Roschach
 echo Please Login to your RDP!!
 ping -n 10 127.0.0.1 >nul
